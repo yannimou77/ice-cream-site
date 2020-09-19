a4b26a9493c1698cd7c539e1520c959920f15dcb
Yanni Mouzakis <yannimou@gmail.com>
2020-09-19 13:46:47 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+    TEST
+    </v-app-bar>
+
+    <v-main>
+      <div>Yoooo</div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+
+  data: () => ({
+    //
+  }),
+};
+</script>

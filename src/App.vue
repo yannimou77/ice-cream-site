af921d3105f3ccac3dc3927c3351240bef8d52d3
Yanni Mouzakis <yannimou@gmail.com>
2020-09-19 21:23:31 -0400

    I think I should probably git ignore some of these directories

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-
-    TEST
-    </v-app-bar>
-
     <v-main>
-      <div>Yoooo</div>
+      <div class="main-content-container">
+        <v-img :src="require('./logo-white-background.png')"></v-img>
+        <HelloWorld></HelloWorld>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
 
+// import HelloWorld from './components/HelloWorld';
+
 export default {
   name: 'App',
-
+  // components: {HelloWorld},
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style scoped>
+.main-content-container {
+  background:#e46454;
+  display: flex;
+  height: 900px;
+  justify-content: center;
+}
+
+.main-contnet {
+  width: 200px;
+  height: 900px;
+  background: white;
+  margin-top: 100px;
+
+}
+</style>
